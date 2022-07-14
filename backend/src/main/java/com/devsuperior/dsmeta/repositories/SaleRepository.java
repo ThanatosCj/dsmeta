08bb053f4ac56ad0fd0926f88b5a60c9f32c9a82
Eduardo Oliveira <dudex6464@live.com>
2022-07-14 18:45:29 -0300

    API test

---
+++ backend/src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.devsuperior.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.dsmeta.entites.Sales;
+
+public interface SaleRepository extends JpaRepository<Sales, Long>{
+
+}
---
+++ backend/src/main/java/com/devsuperior/dsmeta/service/SaleService.java
@@ -0,0 +1,31 @@
+package com.devsuperior.dsmeta.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.dsmeta.entites.Sales;
+import com.devsuperior.dsmeta.repositories.SaleRepository;
+
+
+
+@Service
+public class SaleService {
+	
+	
+	@Autowired
+	private SaleRepository repository;
+	public List<Sales> findSales() {
+		
+		return repository.findAll();
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+}
