bc8a75b0bd4db9a2e35beb8d6a7abaa3baafdc2f
Eduardo Oliveira <dudex6464@live.com>
2022-07-14 22:15:55 -0300

    Hiroko erro Teste

--- backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,4 +1,4 @@
-package com.devsuperior.dsmeta.service;
+package com.devsuperior.dsmeta.services;
 
 import java.time.Instant;
 import java.time.LocalDate;
@@ -9,31 +9,22 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import com.devsuperior.dsmeta.entites.Sales;
+import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.repositories.SaleRepository;
-
-
 
 @Service
 public class SaleService {
-	
-	
+
 	@Autowired
 	private SaleRepository repository;
-	public Page<Sales> findSales(String minDate , String maxDate , Pageable pageable ) {
+	
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+		
 		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		
 		LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
 		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
 		
-		
-		return  repository.findSales(min, max ,pageable );
-		
-		
-		
-		
-		
+		return repository.findSales(min, max, pageable); 
 	}
-	
-	
-	
 }
